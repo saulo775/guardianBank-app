1a22917b601d2a9cf7e3822d1d5681eb86185662
saulo775 <59702227+saulo775@users.noreply.github.com>
2022-10-09 11:31:36 -0300

    style: init project

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Welcome } from './src/Components/Welcome';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Welcome title='hello world'/>
     </View>
   );
 }
@@ -14,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ee1d1d',
     alignItems: 'center',
     justifyContent: 'center',
   },
